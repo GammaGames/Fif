ae201c0c3632daa48280e1b6425951801426da63
GammaGames <jesse007@ymail.com>
2019-11-13 23:28:11 -0700

    restructure and prep for labels and jumping

--- fif.py
+++ fif.py
@@ -6,10 +6,11 @@ import grapheme
 # pop() -> stack
 # pop(0) -> queue
 
-debug_args = ["-d", "--debug"]
-debug = not set(sys.argv).isdisjoint(debug_args)
-args = [arg for arg in sys.argv if arg not in debug_args][1:]
+DEBUG_ARGS = ["-d", "--debug"]
+debug = not set(sys.argv).isdisjoint(DEBUG_ARGS)
+args = [arg for arg in sys.argv if arg not in DEBUG_ARGS][1:]
 stack = []
+labels = {}
 
 
 primary = {
@@ -35,92 +36,111 @@ def _print_debug(length, message=None, include_stack=False):
             print(f"{str(length).ljust(3)}{str(message)}")
 
 
-def _putc(value):
+def _putc(line, length, index):
     print(chr(stack.pop()), end="")
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _putn(value):
+def _putn(line, length, index):
     print(stack.pop(), end="")
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _getn(value):
+def _getn(line, length, index):
     stack.append(int(input()))
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _gets(value):
+def _gets(line, length, index):
     for ch in input():
         stack.append(ord(ch))
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-# label
-# jump
+def _lbl(line, length, index):
+    _print_debug(length)
+    return "lbl"
+
+
+def __lbl(line, length, index):
+    labels[line] = index
+    _print_debug(length)
+
+
+def _jmp(line, length, index):
+    _print_debug(length)
+    return "jmp"
+
+
+def __jmp(line, length, index):
+    pass
+    # TODO figure out best way to return new index values
+    # return labels[line]
+
+
 # jump if zero
 # jump if neg
 
 
-def _exit(value):
-    _print_debug(value)
+def _exit(line, length, index):
+    _print_debug(length)
     quit()
 
 
-def _add(value):
+def _add(line, length, index):
     stack.append(stack.pop() + stack.pop())
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _sub(value):
+def _sub(line, length, index):
     right = stack.pop()
     stack.append(stack.pop() - right)
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _mul(value):
+def _mul(line, length, index):
     stack.append(stack.pop() * stack.pop())
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _div(value):
+def _div(line, length, index):
     right = stack.pop()
     stack.append(math.floor(stack.pop() / right))
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _mod(value):
+def _mod(line, length, index):
     right = stack.pop()
     stack.append(math.floor(stack.pop() % right))
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _push(value):
-    _print_debug(value)
+def _push(line, length, index):
+    _print_debug(length)
     return "push"
 
 
-def __push(value):
-    stack.append(value)
-    _print_debug(value, "", True)
+def __push(line, length, index):
+    stack.append(length)
+    _print_debug(length, "", True)
 
 
-def _dup(value):
+def _dup(line, length, index):
     stack.append(stack[-1])
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _swp(value):
+def _swp(line, length, index):
     first = stack.pop()
     second = stack.pop()
     stack.append(first)
     stack.append(second)
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
-def _pop(value):
+def _pop(line, length, index):
     stack.pop()
-    _print_debug(value, include_stack=True)
+    _print_debug(length, include_stack=True)
 
 
 commands = {
@@ -128,6 +148,10 @@ commands = {
     2: _putn,
     4: _getn,
     5: _gets,
+    6: _lbl,
+    "lbl": __lbl,
+    7: _jmp,
+    "jmp": __jmp,
     11: _add,
     12: _sub,
     13: _mul,
@@ -141,11 +165,24 @@ commands = {
 }
 
 
-with open(args[-1], encoding="utf8") as f:
+def process(program):
     command = None
-    for line in f:
+    for index in range(len(program)):
+        line = program[index]
         length = grapheme.length(line.rstrip("\n"))
         if length in commands and command is None:
-            command = commands[length](length)
+            command = commands[length](line, length, index)
         elif command in commands:
-            command = commands[command](length)
+            command = commands[command](line, length, index)
+
+
+def main():
+    program = []
+    with open(args[-1], encoding="utf8") as f:
+        for line in f:
+            program.append(line)
+    process(program)
+
+
+if __name__ == '__main__':
+    main()
