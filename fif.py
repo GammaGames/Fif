18274bb281350acdaba63c2d3a2ce5b733f81dee
GammaGames <jesse007@ymail.com>
2020-05-09 01:22:32 -0600

    add type hints

--- fif.py
+++ fif.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List, Optional
 import inspect
 import math
 import grapheme
@@ -8,10 +9,10 @@ import click
 # pop(0) -> queue
 
 
-class Fif():
-    def __init__(self, debug=False):
+class Fif:
+    def __init__(self, debug: bool = False):
         self.debug = debug
-        self.program = None
+        self.program: List[str] = []
         self.index = 0
         self.labels = {}
         self.program_name = "Default"
@@ -19,13 +20,8 @@ class Fif():
         self.stacks = {
             self.stack_key: [],
         }
-        self.stack_indexes = {
-            self.stack_key: -1
-        }
-        self.parse = {
-            6: self._parse_labl,
-            "set_labl": self._set_labl
-        }
+        self.stack_indexes = {self.stack_key: -1}
+        self.parse = {6: self._parse_labl, "set_labl": self._set_labl}
         self.commands = {
             1: self._putc,
             2: self._putn,
@@ -48,7 +44,7 @@ class Fif():
             17: self._dup,
             18: self._swap,
             19: self._pop,
-            "noop": self._noop
+            "noop": self._noop,
         }
 
     def _print_debug(self, length, message=None, include_stack=False):
@@ -58,114 +54,122 @@ class Fif():
                 if caller.startswith("_"):
                     message = caller[1:]
 
-            line_number = f"line {str(self.index + 1).rjust(len(str(len(self.program))))}"
+            line_number = (
+                f"line {str(self.index + 1).rjust(len(str(len(self.program))))}"
+            )
             text = f"{line_number}: {str(length).ljust(3)}{str(message).ljust(6)}"
             if include_stack:
-                print(f"{text} - [{', '.join(str(x) for x in self.stacks[self.stack_key])}]")
+                print(
+                    f"{text} - [{', '.join(str(x) for x in self.stacks[self.stack_key])}]"
+                )
             else:
                 print(text)
 
-    def _putc(self, line, length, index):
+    def _putc(self, line: str, length: int, index: int):
         pop_index = self.stack_indexes[self.stack_key]
         popped = chr(self.stacks[self.stack_key].pop(pop_index))
         if not self.debug:
             print(popped, end="")
         self._print_debug(length, message=f"putc {popped}", include_stack=True)
 
-    def _putn(self, line, length, index):
+    def _putn(self, line: str, length: int, index: int):
         pop_index = self.stack_indexes[self.stack_key]
         popped = self.stacks[self.stack_key].pop(pop_index)
         if not self.debug:
             print(popped, end="")
         self._print_debug(length, message=f"putn {popped}", include_stack=True)
 
-    def _getn(self, line, length, index):
+    def _getn(self, line: str, length: int, index: int):
         self.stacks[self.stack_key].append(int(input()))
         self._print_debug(length, include_stack=True)
 
-    def _gets(self, line, length, index):
+    def _gets(self, line: str, length: int, index: int):
         for ch in input():
             self.stacks[self.stack_key].append(ord(ch))
         self._print_debug(length, include_stack=True)
 
-    def _labl(self, line, length, index):
+    def _labl(self, line: str, length: int, index: int):
         self._print_debug(length)
         return "labl"
 
-    def __labl(self, line, length, index):
+    def __labl(self, line: str, length: int, index: int):
         # Essentally a noop
         pass
 
-    def _jump(self, line, length, index):
+    def _jump(self, line: str, length: int, index: int):
         self._print_debug(length)
         return "do_jump"
 
-    def _jz(self, line, length, index):
+    def _jz(self, line: str, length: int, index: int):
         check = self.stacks[self.stack_key][-1] == 0
-        self._print_debug(length, message=f"jz {'T' if check else 'F'}", include_stack=True)
+        self._print_debug(
+            length, message=f"jz {'T' if check else 'F'}", include_stack=True
+        )
         return "do_jump" if check else "noop"
 
-    def _jn(self, line, length, index):
+    def _jn(self, line: str, length: int, index: int):
         check = self.stacks[self.stack_key][-1] < 0
-        self._print_debug(length, message=f"jn {'T' if check else 'F'}", include_stack=True)
+        self._print_debug(
+            length, message=f"jn {'T' if check else 'F'}", include_stack=True
+        )
         return "do_jump" if check else "noop"
 
-    def _do_jump(self, line, length, index):
+    def _do_jump(self, line: str, length: int, index: int):
         self.index = self.labels[line]
         self._print_debug(self.index, "jump")
 
-    def _exit(self, line, length, index):
+    def _exit(self, line: str, length: int, index: int):
         self._print_debug(length, "exit")
         quit()
 
-    def _add(self, line, length, index):
+    def _add(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         left = self.stacks[self.stack_key].pop(pop_index)
         right = self.stacks[self.stack_key].pop(pop_index)
         self.stacks[self.stack_key].append(left + right)
         self._print_debug(length, include_stack=True)
 
-    def _sub(self, line, length, index):
+    def _sub(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         right = self.stacks[self.stack_key].pop(pop_index)
         left = self.stacks[self.stack_key].pop(pop_index)
         self.stacks[self.stack_key].append(left - right)
         self._print_debug(length, include_stack=True)
 
-    def _mul(self, line, length, index):
+    def _mul(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         left = self.stacks[self.stack_key].pop(pop_index)
         right = self.stacks[self.stack_key].pop(pop_index)
         self.stacks[self.stack_key].append(left * right)
         self._print_debug(length, include_stack=True)
 
-    def _div(self, line, length, index):
+    def _div(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         right = self.stacks[self.stack_key].pop(pop_index)
         left = self.stacks[self.stack_key].pop(pop_index)
         self.stacks[self.stack_key].append(math.floor(left / right))
         self._print_debug(length, include_stack=True)
 
-    def _mod(self, line, length, index):
+    def _mod(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         right = self.stacks[self.stack_key].pop(pop_index)
         left = self.stacks[self.stack_key].pop(pop_index)
         self.stacks[self.stack_key].append(math.floor(left % right))
         self._print_debug(length, include_stack=True)
 
-    def _push(self, line, length, index):
+    def _push(self, line: str, length: int, index: int) -> str:
         self._print_debug(length)
         return "push"
 
-    def __push(self, line, length, index):
+    def __push(self, line: str, length: int, index: int) -> None:
         self.stacks[self.stack_key].append(length)
         self._print_debug(length, "", True)
 
-    def _dup(self, line, length, index):
+    def _dup(self, line: str, length: int, index: int) -> None:
         self.stacks[self.stack_key].append(self.stacks[self.stack_key][-1])
         self._print_debug(length, include_stack=True)
 
-    def _swap(self, line, length, index):
+    def _swap(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         first = self.stacks[self.stack_key].pop(pop_index)
         second = self.stacks[self.stack_key].pop(pop_index)
@@ -173,46 +177,42 @@ class Fif():
         self.stacks[self.stack_key].append(second)
         self._print_debug(length, include_stack=True)
 
-    def _pop(self, line, length, index):
+    def _pop(self, line: str, length: int, index: int) -> None:
         pop_index = self.stack_indexes[self.stack_key]
         self.stacks[self.stack_key].pop(pop_index)
         self._print_debug(length, include_stack=True)
 
-    def _stck(self, line, length, index):
+    def _stck(self, line: str, length: int, index: int):
         pass
 
-    def _noop(self, line, length, index):
+    def _noop(self, line: str, length: int, index: int) -> None:
         self._print_debug(length)
 
-    def _parse_labl(self, line, length, index):
+    def _parse_labl(self, line: str, length: int, index: int) -> str:
         return "set_labl"
 
-    def _set_labl(self, line, length, index):
+    def _set_labl(self, line: str, length: int, index: int) -> None:
         self.labels[line] = index
 
-    def preprocess(self):
+    def preprocess(self) -> None:
         self.labels = {}
-        command = None
+        command: Optional[str] = None
 
         for index in range(len(self.program)):
-            line = self.program[index].rstrip("\n")
-            length = grapheme.length(line.rstrip("\n"))
+            line: str = self.program[index].rstrip("\n")
+            length: int = grapheme.length(line.rstrip("\n"))
             command_length = length % 32
             if command_length in self.parse and command is None:
                 command = self.parse[command_length](line, length, index)
             elif command in self.parse:
                 command = self.parse[command](line, length, index)
 
-    def process(self):
+    def process(self) -> None:
         self.index = 0
         self.stack_key = self.program_name
-        self.stacks = {
-            self.stack_key: []
-        }
-        self.stack_indexes = {
-            self.stack_key: -1
-        }
-        command = None
+        self.stacks = {self.stack_key: []}
+        self.stack_indexes = {self.stack_key: -1}
+        command: Optional[str] = None
 
         while self.index < len(self.program):
             line = self.program[self.index].rstrip("\n")
@@ -224,7 +224,7 @@ class Fif():
                 command = self.commands[command](line, length, self.index)
             self.index += 1
 
-    def execute(self, program, program_name=None):
+    def execute(self, program: list, program_name: str = None) -> None:
         self.program = program
         if program_name is not None:
             self.program_name = program_name
@@ -232,14 +232,14 @@ class Fif():
         self.process()
 
 
-def execute(program, program_name=None, debug=False):
+def execute(program: List[str], program_name: str = None, debug: bool = False) -> None:
     fif = Fif(debug)
     fif.execute(program, program_name)
 
 
 @click.command()
 @click.option("--debug", "-d", is_flag=True, help="Print debug information")
-@click.argument('filename', type=click.File("r"))
+@click.argument("filename", type=click.File("r"))
 def main(debug, filename):
     program_name = Path(filename.name).resolve().stem
     execute(filename.readlines(), program_name, debug=debug)
